6663d21f9408cc54bf291726643c9b1811acd8bc
rubenambarita <rubenambarita7@gmail.com>
2021-08-31 22:30:21 +0700

    add

--- cypress/integration/sprint 5 - day 2/upload.js
+++ cypress/integration/sprint 5 - day 2/upload.js
@@ -24,25 +24,33 @@ describe("kumpulan test case", function () {
       .type("6212345678")
       .should("have.value", "6212345678");
 
-    cy.get(".subjects-auto-complete__value-container", { force: true }).type(
-      "tes"
-    );
+    cy.get("#dateOfBirthInput").click({ force: true });
+    cy.get(".react-datepicker__navigation--next").click();
+    cy.get(".react-datepicker__day--015").click({ force: true });
+    cy.get("#dateOfBirthInput")
+      .invoke("val")
+      .then((text) => {
+        expect("15 Sep 2021").to.equal(text);
+      });
+
+    cy.get(".subjects-auto-complete__value-container")
+      .click()
+      .focused()
+      .type("Hindi" + "{downarrow}{enter}", { force: true });
+    cy.contains("Hindi");
 
     cy.get('[value="1"]').check({ force: true }).should("have.value", "1");
 
     cy.get("#uploadPicture").attachFile("Picture5.png");
-    cy.get("#currentAddress", { force: true }).type("Haloooooo");
+    cy.get("#currentAddress", { force: true })
+      .type("Halo")
+      .should("have.value", "Halo");
+
+    cy.get("#stateCity-wrapper > :nth-child(2)").click();
+    cy.get("#react-select-3-option-0").click();
+    cy.get("#city").click();
+    cy.get("#react-select-4-option-1").click();
+    cy.get(".css-1uccc91-singleValue").should("have.text", "NCRGurgaon");
     cy.contains("Submit").click();
   });
-  //     describe("upload-file", function () {
-  //       it("upload file", function () {
-  //         cy.visit("https://admin.pkh.dojobox.id/");
-  //         cy.get("#input-email").type("e@dojobox.id");
-  //         cy.get("#input-password").type("admin");
-  //         cy.get("#btn-login").click();
-  //         cy.get(".d-flex > .btn").click();
-  //         cy.get('input[type="file"]').attachFile("Picture5.png");
-  //       });
-  //     });
-  // ;
 });
